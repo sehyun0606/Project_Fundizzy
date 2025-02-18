c446d2b97acfd6aebd9afab1feea7d12e5ae4a0e
이재영 <LJY>
2025-02-19 03:01:53 +0900

    마이페이지 계좌정보관리 ui

---
+++ Project_Fundizzy/src/main/java/com/itwillbs/project_fundizzy/controller/BankController.java
@@ -0,0 +1,14 @@
+package com.itwillbs.project_fundizzy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BankController {
+	
+	// 마이페이지 - 결제정보관리 페이지
+	@GetMapping("ManageMyPaymentInfo")
+	public String manageMyPaymentInfo() {
+		return "bank/my_payment_info_manage";
+	}
+}
