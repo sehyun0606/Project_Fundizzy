e6fd7f67812a6cd459c56b708e4603835e72a387
최서해 <CSH>
2025-03-06 20:34:54 +0900

    새소식 수정 삭제 구현, 차트 크기 수정

--- Project_Fundizzy/src/main/java/com/itwillbs/project_fundizzy/mapper/ProjectStateMapper.java
+++ Project_Fundizzy/src/main/java/com/itwillbs/project_fundizzy/mapper/ProjectStateMapper.java
@@ -4,14 +4,25 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.itwillbs.project_fundizzy.vo.NewsVO;
 
 @Mapper
 public interface ProjectStateMapper {
 
-	int insertNewsBoard(NewsVO news);
+	int insertNewsBoard(Map<String, String> map);
 
-	List<NewsVO> selectNewsList(String project_code);
+	List<NewsVO> selectNewsList(@Param("project_code")String project_code, @Param("startRow")int startRow, @Param("listLimit")int listLimit);
+
+	int selectNewsListCount(String project_code);
+
+	NewsVO getNews(int news_code);
+
+	int deleteNews(int news_code);
+
+	int updateNewsBoard(Map<String, String> map);
+
+
 
 }
