f591f5f2f5d807dd580832cb4fba215f76e2358c
jyoun <jyoun@DESKTOP-TQLDHBM>
2025-03-10 21:42:47 +0900

    vo 추가

--- Project_Fundizzy/src/main/java/com/itwillbs/project_fundizzy/vo/FundizzyPay.java
+++ Project_Fundizzy/src/main/java/com/itwillbs/project_fundizzy/vo/FundizzyPay.java
@@ -2,9 +2,13 @@ package com.itwillbs.project_fundizzy.vo;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FundizzyPay implements Serializable {
 	private String email;
 	private int pay_amt;
